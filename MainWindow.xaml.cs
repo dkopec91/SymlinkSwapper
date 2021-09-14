44b8619bb145277f9d96d424e59b10203b216cff
dkopec91 <dkopec91@gmail.com>
2021-09-14 02:50:09 +0200

    Minimizing to the system tray

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
 using System.Windows.Input;
 
 namespace SymlinkSwapper
@@ -8,16 +11,33 @@ namespace SymlinkSwapper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
             this.MouseDown += Window_MouseDown;
+
+            _notifyIcon = new()
+            {
+                Text = "SymlinkSwapper",
+                BalloonTipTitle = "SymlinkSwapper",
+                BalloonTipText = "The app has been minimised. Click the tray icon to show.",
+                Icon = new Icon(Properties.Resources.AppIcon, new System.Drawing.Size(16, 16))
+            };
+            _notifyIcon.Click += new EventHandler(_notifyIcon_Click);
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
+            Hide();
+            if (_notifyIcon != null)
+                _notifyIcon.ShowBalloonTip(500);
+        }
+
+        private void _notifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -25,14 +45,22 @@ namespace SymlinkSwapper
             Close();
         }
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        private void OnClose(object sender, CancelEventArgs args)
         {
-
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
         }
 
-        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
         {
+            if (_notifyIcon != null)
+                _notifyIcon.Visible = !IsVisible;
+        }
 
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
         }
     }
 }
